20587d511ac69fb06cc65ff3584450b31602ef2f
Brandon Hoffman <branhoff2@gmail.com>
2024-01-21 22:30:05 -0800

    refactoring to use a single form so response from GPT is on the same page

--- app/routes/form_routes.py
+++ app/routes/form_routes.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, request, render_template, g
-from app.services.chat_gpt_service import prompt_chat_gpt
+from flask import Blueprint, request, jsonify, render_template, g
 from app.utils.util import get_context
 
 form_bp = Blueprint('form_bp', __name__)
@@ -10,25 +9,19 @@ def home():
 
 @form_bp.route('/submit_diagnostic', methods=['POST'])
 def submit_diagnostic():
-    make = request.form.get('make')
-    model = request.form.get('model')
-    year = request.form.get('year')
-    car_issue = request.form.get('carIssue')
-
     g.make = request.form.get('make')
     g.model = request.form.get('model')
     g.year = request.form.get('year')
     g.car_issue = request.form.get('carIssue')
 
-    # message_content = f"Car Make: {make}, Model: {model}, Year: {year}, Issue: {car_issue}"
-
-    # response = prompt_chat_gpt(message_content)
-
     chat_gpt_response = get_context()
 
-    return render_template('diagnostic_submitted.html', chat_gpt_response=chat_gpt_response, diagnostic_info={
-        'make': make,
-        'model': model,
-        'year': year,
-        'car_issue': car_issue
+    return jsonify({
+        'chat_gpt_response': chat_gpt_response,
+        'diagnostic_info': {
+            'make': g.make,
+            'model': g.model,
+            'year': g.year,
+            'car_issue': g.car_issue
+        }
     })
