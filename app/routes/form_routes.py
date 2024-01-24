2533a29a42a88e9e5d6a2bc98788878df0a3f1d1
Brandon Hoffman <branhoff2@gmail.com>
2024-01-23 23:25:40 -0800

    Instead of using Flask's global g object for storing and passing make, model, year, and car_issue, these details are now passed directly as parameters to relevant functions.

--- app/routes/form_routes.py
+++ app/routes/form_routes.py
@@ -3,26 +3,27 @@ from app.utils.util import get_context
 
 form_bp = Blueprint('form_bp', __name__)
 
+
 @form_bp.route('/')
 def home():
     return render_template('form.html')
-  
+
 
 @form_bp.route('/submit_diagnostic', methods=['POST'])
 def submit_diagnostic():
-    g.make = request.form.get('make')
-    g.model = request.form.get('model')
-    g.year = request.form.get('year')
-    g.car_issue = request.form.get('carIssue')
+    make = request.form.get('make')
+    model = request.form.get('model')
+    year = request.form.get('year')
+    car_issue = request.form.get('carIssue')
 
-    chat_gpt_response = get_context()
+    chat_gpt_response = get_context(make, model, year, car_issue)
 
     return jsonify({
         'chat_gpt_response': chat_gpt_response,
         'diagnostic_info': {
-            'make': g.make,
-            'model': g.model,
-            'year': g.year,
-            'car_issue': g.car_issue
+            'make': make,
+            'model': model,
+            'year': year,
+            'car_issue': car_issue
         }
     })
