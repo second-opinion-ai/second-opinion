40e44c78a422480c8d1f026f0c4fe0e35229933a
Brandon Hoffman <branhoff2@gmail.com>
2024-01-27 15:06:26 -0800

    adding some initial installation pieces and the first couple of jupyter notebooks with instructions on finetuning

---
+++ install_finetuning_requirements.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Check Python version
+if ! python -c 'import sys; assert sys.version_info >= (3,9)' > /dev/null; then
+    echo "Python version should be >= 3.9"
+    exit 1
+fi
+
+# Update Python resources at the global level
+python -m pip install --upgrade setuptools wheel pip
+
+# Setup and activate virtual environment
+python -m venv .venv
+source .venv/bin/activate
+if [ $? -ne 0 ]; then
+    echo "Failed to activate the virtual environment."
+    exit 1
+fi
+
+python -m pip install --upgrade setuptools wheel pip
+
+# Check and install requirements
+if [ -f "requires/finetuning.txt" ]; then
+    python -m pip install -r requires/finetuning.txt
+fi
+
+#if [ -f "requires/dev.txt" ]; then
+#    python -m pip install -r requires/dev.txt
+#fi
